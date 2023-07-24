c9942eb28c5405283a72bd24b3f2e62f80d14193
Андрей <chosenfew@yandex.ru>
2023-07-24 20:32:56 +0300

    Домашка 21.1 часть 2

--- blogs/views.py
+++ blogs/views.py
@@ -59,4 +59,5 @@ class BlogUpdateView(UpdateView):
 
 class BlogDeleteView(DeleteView):
     model = Blog
+    template_name = 'blogs/blog_confirm_delete.html'
     success_url = reverse_lazy('blogs:list')
